937dbdd8c9bc58d2d2d13ab031906798c48696ec
WangJibo <w5571393#>
2021-02-22 16:27:04 +0800

    1.监控异常传输异常

--- pos/monitor/monitor_pos.py
+++ pos/monitor/monitor_pos.py
@@ -20,16 +20,17 @@ headers["Content-Type"] = content_type
 # 监控提示
 address3 = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=cedf5cd7-9ee7-4b59-8b4a-d050a3877875"
 
+
 # 获得token
-# 获取登录企业之后的token
-def get_company_token(parameter="pro_login"):
+def get_company_token(parameter):
+    # 获得集团token
     login_body = ReadConfig.read_config(parameter)["login"]
     test_url = ReadConfig.read_config(parameter)["address"] + "/auth/authorization/auth"
     header = {"Content-Type": "application/json; charset=UTF-8"}
     response = requests.post(test_url, data=json.dumps(login_body), headers=header)
     text = response.text
     login_access_token = json.loads(text)['data']['accessToken']
-
+    # 获得企业token
     test_url = ReadConfig.read_config(parameter)["address"] + "/auth/authorization/change_company"
     header = {"Content-Type": "application/json; charset=UTF-8"}
     header.update({"access-token": login_access_token})
@@ -61,35 +62,48 @@ def send_to_wx(content):
     requests.post(url=address3, data=json.dumps(data1), headers=headers)
 
 
+# 查询数据
+def do_monitor(group_name, test_url, headers):
+    data = {"pageIndex": 1, "pageSize": 100, "sortBy": "", "sortColumn": "", "serialNumber": 0,
+            "t_pos_monitor__company_id___t_org_organization_base__org_name": "",
+            "t_pos_monitor__business_id___t_org_organization_base__org_name": "",
+            "t_pos_monitor__online_status": [], "t_pos_monitor__pos_num": "", "t_pos_monitor__pos_version": "",
+            "t_pos_monitor__update_job_exist": [], "t_pos_monitor__status": [], "t_pos_monitor__pos_time_diff": "",
+            "t_pos_monitor__pos_sys_time": ["", ""], "t_pos_monitor__create_user": [],
+            "t_pos_monitor__create_time": ["", ""], "t_pos_monitor__modify_user": [],
+            "t_pos_monitor__modify_time": ["", ""], "listId": 719, "listViewId": 1002828,
+            "_functionCode": "POSJKZXGN"}
+    response = requests.post(test_url, data=json.dumps(data), headers=headers)
+    # print(json.loads(response.text)["dataList"])
+    for obj in json.loads(response.text)["dataList"]:
+        status = (obj["t_pos_monitor__status_value"])
+        if status == str(2) and obj["t_pos_monitor__online_status"] == '是':
+            send_to_wx((obj["t_pos_monitor__business_id___t_org_organization_base__org_name"]) + "传输异常 !!")
+    fmtTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(fmtTime + group_name + " 检测完成 !!!")
+
+
 if __name__ == '__main__':
+
+    test_url = ReadConfig.read_config("pro_login")["address"] + "/basedata/list/view/query/new/981"
+
     # 获得力东token
     token, address = get_company_token("pro_login")
-
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
         'access-token': token
     }
 
+    # 获得绿森林token
+    token2, address2 = get_company_token("pro_login2")
+    headers2 = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'access-token': token2
+    }
+
     while 1:
-        # 查看监控信息 https://haidianyun.com/gateway/basedata/list/view/query/new/1002828
-        test_url = ReadConfig.read_config("pro_login")["address"] + "/basedata/list/view/query/new/1002828"
-
-        data = {"pageIndex": 1, "pageSize": 50, "sortBy": "", "sortColumn": "", "serialNumber": 0,
-                "t_pos_monitor__company_id___t_org_organization_base__org_name": "",
-                "t_pos_monitor__business_id___t_org_organization_base__org_name": "",
-                "t_pos_monitor__online_status": [], "t_pos_monitor__pos_num": "", "t_pos_monitor__pos_version": "",
-                "t_pos_monitor__update_job_exist": [], "t_pos_monitor__status": [], "t_pos_monitor__pos_time_diff": "",
-                "t_pos_monitor__pos_sys_time": ["", ""], "t_pos_monitor__create_user": [],
-                "t_pos_monitor__create_time": ["", ""], "t_pos_monitor__modify_user": [],
-                "t_pos_monitor__modify_time": ["", ""], "listId": 719, "listViewId": 1002828,
-                "_functionCode": "POSJKZXGN"}
-
-        response = requests.post(test_url, data=json.dumps(data), headers=headers)
-        # print(json.loads(response.text)["dataList"])
-        for obj in json.loads(response.text)["dataList"]:
-            status = (obj["t_pos_monitor__status_value"])
-            if status == str(2) and obj["t_pos_monitor__online_status"] == '是':
-                send_to_wx((obj["t_pos_monitor__business_id___t_org_organization_base__org_name"]) + "传输异常 !!")
-        fmtTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(fmtTime + " 检测完成 !!!")
+        # 力东查看监控信息
+        do_monitor("力东", test_url, headers)
+        # 绿森林
+        do_monitor("绿森林", test_url, headers2)
         sleep(60 * 5)
